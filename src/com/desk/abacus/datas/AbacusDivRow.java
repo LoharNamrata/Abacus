7d2df752f7a745dfbd22184e4513a402a417362f
DESKTOP-HKLVJJ5\Admin <namratalohar@pragiti.com>
2020-10-15 19:57:14 +0530

    desk.abacus deleted files

--- src/com/desk/abacus/datas/AbacusDivRow.java
+++
@@ -1,7 +0,0 @@
-package com.desk.abacus.datas;
-
-import java.util.Collection;
-
-public interface AbacusDivRow {
-	public Collection<String> division(int dividentStart, int dividentEnd, int devisorStart, int devisorEnd);
-}
--- src/com/desk/abacus/datas/AbacusMulRow.java
+++
@@ -1,8 +0,0 @@
-package com.desk.abacus.datas;
-
-import java.util.Collection;
-
-public interface AbacusMulRow {
-	public Collection<String> multiplication(int multiplicantStart, int multiplicantEnd, int multiplierStart,
-			int multiplierEnd);
-}
